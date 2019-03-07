01c2c93dd15ff7a0821ec1412532f6718d610a31
joshnos <joshuanostas49@gmail.com>
2019-03-07 00:20:19 -0400

    delete unnecessary variables

--- statement.js
+++ statement.js
@@ -6,7 +6,6 @@ function statement (invoice, plays) {
                       { style: "currency", currency: "USD",
                         minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -15,8 +14,8 @@ function statement (invoice, plays) {
       volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
   }
 
   result += `Amount owed is ${format(totalAmount/100)}\n`;
